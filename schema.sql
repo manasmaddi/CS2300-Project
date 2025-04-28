ff44473525adb69426c405e8cb139d9da27b77b9
manasmaddi <148016813+manasmaddi@users.noreply.github.com>
2025-04-27 22:51:29 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,11 +11,13 @@ CREATE TABLE "User" (
     userID SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, 
     height INT,
     startingWeight FLOAT,
     currentWeight FLOAT,
     goalWeight FLOAT
 );
+
 
 -- Authentication table
 CREATE TABLE Authentication (
